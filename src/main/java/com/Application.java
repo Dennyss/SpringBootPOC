cf338251ccf9b819b933900eb121beb4feca418e
Denys Kovalenko <denis.kovalenko@yahoo.com>
2018-05-23 11:22:57 -0500

    The showcase demonstrates usage of properties injection without @Value and property name.

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -1,18 +1,19 @@
 package com;
 
-import com.configuration.SpringConfiguration;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
- * Created by Denys Kovalenko on 9/30/2014.
+ * Created by Denys Kovalenko on 5/23/2018.
  */
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
+    private static final Logger LOGGER = LogManager.getLogger(Application.class);
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(SpringConfiguration.class, args);
+        SpringApplication.run(Application.class, args);
+        LOGGER.info("Application stated successfully!");
     }
 }
--- src/main/java/com/configuration/SpringConfiguration.java
+++ src/main/java/com/configuration/SpringConfiguration.java
@@ -1,60 +1,29 @@
 package com.configuration;
 
-import com.jms.JMSMessageConsumer;
-import com.jms.JMSMessageProducer;
-import com.redis.RedisDAOImpl;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.*;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import reactor.core.Environment;
-import reactor.core.Reactor;
-import reactor.core.spec.Reactors;
-import reactor.spring.context.config.EnableReactor;
-import redis.clients.jedis.JedisShardInfo;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by Denys Kovalenko on 10/6/2014.
  */
-@Configuration
-@EnableReactor
-@EnableAutoConfiguration
-public class SpringConfiguration {
-    @Value("${redis.host}")
-    private String host;
+@Component
+@ConfigurationProperties(prefix="connection")
+public class SpringConfiguration implements InitializingBean {
+    private static final Logger LOGGER = LogManager.getLogger(SpringConfiguration.class);
 
-    @Value("${redis.port}")
-    private String port;
+    //@Value("${connection-redisHost}")
+    private String redisHost;
 
-    @Bean
-    Reactor createReactor(Environment env) {
-        return Reactors.reactor()
-                .env(env)
-                .dispatcher(Environment.RING_BUFFER)
-                .get();
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        LOGGER.info("The property value host is: " + redisHost);
     }
 
-    @Bean
-    @Scope("singleton")
-    public JedisConnectionFactory getJedisConnectionFactory(){
-        return new JedisConnectionFactory(new JedisShardInfo(host, port));
+    public void setRedisHost(String redisHost) {
+        this.redisHost = redisHost;
     }
-
-    @Bean
-    @Scope("singleton")
-    public RedisDAOImpl getRedisDAO(){
-        return new RedisDAOImpl(getJedisConnectionFactory());
-    }
-
-    @Bean(name = "jmsMessageConsumer")
-    public JMSMessageConsumer createJMSMessageConsumer(){
-        return new JMSMessageConsumer();
-    }
-
-    @Bean
-    @DependsOn("jmsMessageConsumer")
-    public JMSMessageProducer createJMSMessageProducer(){
-        return new JMSMessageProducer();
-    }
-
 }
